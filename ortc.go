3e4d0567f77f28cec8f7c24b151ff2e9a174983d
shynome <shynome@gmail.com>
2023-04-11 23:07:32 +0800

    split from shynome/wl

---
+++ ortc.go
@@ -0,0 +1,103 @@
+package ortc
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/lainio/err2"
+	"github.com/lainio/err2/try"
+	"github.com/pion/webrtc/v3"
+)
+
+type Signal webrtc.SessionDescription
+
+func CreateOffer(pc *webrtc.PeerConnection) (sdp Signal, err error) {
+	defer err2.Handle(&err)
+
+	try.To(makeOfferWithCandidates(pc))
+
+	offer := try.To1(pc.CreateOffer(nil))
+
+	try.To(pc.SetLocalDescription(offer))
+	sdp = Signal(*pc.LocalDescription())
+
+	return
+}
+
+func makeOfferWithCandidates(pc *webrtc.PeerConnection) (err error) {
+	defer err2.Handle(&err)
+
+	dc := try.To1(pc.CreateDataChannel("_for_collect_candidates", nil))
+	defer dc.Close()
+
+	wait := make(chan struct{})
+	pc.OnNegotiationNeeded(func() {
+		close(wait)
+	})
+	<-wait
+
+	return
+}
+
+func HandleConnect(pc *webrtc.PeerConnection, offer Signal) (roffer Signal, err error) {
+	defer err2.Handle(&err)
+
+	try.To(pc.SetRemoteDescription(webrtc.SessionDescription(offer)))
+
+	answer := try.To1(pc.CreateAnswer(nil))
+
+	gatherComplete := webrtc.GatheringCompletePromise(pc)
+
+	try.To(pc.SetLocalDescription(answer))
+
+	<-gatherComplete
+
+	roffer = Signal(*pc.LocalDescription())
+
+	return
+}
+
+func Handshake(pc *webrtc.PeerConnection, offer Signal) (err error) {
+	defer err2.Handle(&err)
+
+	try.To(pc.SetRemoteDescription(webrtc.SessionDescription(offer)))
+
+	return
+}
+
+var ErrDataChannelClosed = errors.New("DataChannel state is closed")
+
+func Wait(dc *webrtc.DataChannel) (err error) {
+	defer err2.Handle(&err)
+
+	switch dc.ReadyState() {
+	case webrtc.DataChannelStateOpen:
+		return
+	case webrtc.DataChannelStateClosing:
+		fallthrough
+	case webrtc.DataChannelStateClosed:
+		return ErrDataChannelClosed
+	case webrtc.DataChannelStateConnecting:
+		break
+	}
+
+	var (
+		wait = make(chan struct{})
+	)
+
+	dc.OnOpen(func() {
+		close(wait)
+	})
+	// dc.OnError(func(err error) {
+	// 	errCh <- err
+	// })
+
+	select {
+	case <-wait:
+	case <-time.After(10 * time.Second):
+		err = fmt.Errorf("wait dc timeout")
+	}
+
+	return
+}
